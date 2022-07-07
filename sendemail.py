bf59b74a1fe9b66222eceb6e64086a4acfc641cd
madhavsnair <77969860+madhavsnair@users.noreply.github.com>
2022-07-07 20:01:47 +0530

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -6,7 +6,7 @@ def sendmail(TEXT,email,SUBJECT):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.ehlo()
     s.starttls()
-    s.login("memail", "madhav@1234")
+    s.login("memail", "pass")
     message  = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
     s.sendmail("mdhvsnair@gmail.com", email, message)
     s.quit()
